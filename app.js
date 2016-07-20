d89d85f512bc46491a183fb23543c04a370a1a47
Drew Conly <atconly@gmail.com>
2016-07-20 13:38:08 -0600

    swap background set routes create button

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ angular.module('starWars').config(function($routeProvider) {
     $routeProvider.when( '/createCharacter', {
       templateUrl: 'createCharacter.html'
     })
+    $routeProvider.when( '/yodaAdvice', {
+      templateUrl: 'yodaAdvice.html'
+    })
 
     // default route //
     $routeProvider
