a7032a306301a3440f9753ad111f14b762b3a72d
Drew Conly <atconly@gmail.com>
2016-07-20 14:32:21 -0600

    add factory with API call

--- app.js
+++ app.js
@@ -19,3 +19,59 @@ angular.module('starWars').config(function($routeProvider) {
         redirectTo: '/'
       });
 });
+
+// Input Factory //
+
+angular.module('starWars')
+    .factory('InputFactory', [
+        '$http',
+        inputFactory
+    ]);
+
+function inputFactory($http) {
+    var starWarsData = {
+        getData: function (URL) {
+            return $http.get(URL);
+        }
+    };
+
+    return starWarsData;
+}
+
+// Input Controller //
+
+angular.module('starWars')
+    .controller('InputController', [
+        'InputFactory',
+        inputController
+    ]);
+
+// Declare controller function //
+
+function inputController(InputFactory) {
+    console.log('Controller functional, human.');
+    var inputCont = this;
+    var inputFact = InputFactory;
+
+    inputCont.ajaxCall = function(URL) {
+            inputFact.getData(URL).then(function (response) {
+            inputCont.appData = response;
+            console.log('response is: ' + response);
+            // this should be the entire string of each object type that's
+            // called based upon the user inputs
+            // now we need a function to sift through to find the object itself
+            // by name of user input and isolate that object, and push into a master
+            // array for building and showing the view
+        })
+    };
+    inputFact.species = inputCont.ajaxCall('http://swapi.co/api/species/');
+    inputFact.planet = inputCont.ajaxCall('http://swapi.co/api/planets/');
+    inputFact.starship = inputCont.ajaxCall('http://swapi.co/api/starships');
+
+    inputFact.inputSearch = function(userInput, type) {
+        switch(userInput) {
+            case 'planet':
+
+        }
+    }
+}
